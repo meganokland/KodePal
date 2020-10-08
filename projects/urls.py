fbd9ab875b65dcd033f51c26bf49e92e85e294da
megan_o <oklandmegan@gmail.com>
2020-10-08 12:48:07 +0200

    updated password templates

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import ProjectListView, ProjectDetailView, SearchResultsListView
+from .views import (
+    ProjectListView, ProjectDetailView, ProjectUpdateView,
+    ProjectDeleteView,
+    ProjectCreateView,
+    SearchResultsListView,
+)
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
     path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     path('search/', SearchResultsListView.as_view(), name='search_results'),
+
+    path('<int:pk>/edit/', ProjectUpdateView.as_view(), name='project_edit'),
+    path('<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('new/', ProjectCreateView.as_view(), name='project_new'),
 ]
